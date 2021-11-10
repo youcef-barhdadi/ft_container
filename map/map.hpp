ba6892d817e75315a8b9eec9c400ac711e62a13a
youcef <youssef.xd.12@gmail.com>
2021-11-10 17:25:00 +0100

    fdsa

---
+++ map/map.hpp
@@ -0,0 +1,17 @@
+
+# include  <functional>
+
+template<
+    class Key,
+    class T,
+    class Compare = std::less<Key>,
+    class Allocator = std::allocator<std::pair<const Key, T> >
+> class map
+{
+
+        public :
+        
+
+        private :
+
+}
