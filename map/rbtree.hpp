110a661f43e1f4b58c495ebc11b1d25e1b445ea6
Youcef Barhdadi <you@example.com>
2021-11-14 20:37:32 +0100

    dfas

--- map/rbtree.hpp
+++ map/rbtree.hpp
@@ -76,26 +76,46 @@ template<
 
 
 
-            void left_rotaion(node<T> *node)
+            // void left_rotaion(node<T> *node)
+            // {
+
+            //     node<T> *n = node->left;
+
+            //     n->left = node;
+
+
+            //     node->right = node->left->left;
+
+            //     node = n;
+
+            // }
+
+            // void leftRight(node<T> *n)
+            // {
+            //     left_rotaion(n->left);
+            //     right_rotation(n);
+
+
+            // }
+
+            // void right_rotation(node<T> *_node)
+            // {
+            //     node<T> *B = _node->left;
+            //     t->left = node;
+            //     _node->right = B;
+
+            // }
+
+            void check(node<T> *n)
             {
-
-                node<T> *n = node->left;
-
-                n->left = node;
-
-
-                node->right = node->left->left;
-
-                node = n;
-
-            }
-
-            void right_rotation(node<T> *node)
-            {
-                node<T> *B = node->left;
-                t->left = node;
-                node->right = B;
-
+                  if (n == this->root)
+                  {
+                      return ;
+                  }
+                  if ( n->parent != nullptr &&  n->parent->color == Red)
+                  {
+                        std::cout <<  "ff" << std::endl; 
+                  }
             }
 
             node<T>  *_insert(node<T> *_node ,T value)
@@ -113,8 +133,17 @@ template<
                     }
                     if (_node->value >= value)
                     {
-                       _node->left =   _insert(_node->left, value);
-                       _node->left->parent = _node->left;
+                        node<T> *temp  =   _insert(_node->left, value);
+
+                        if (_node->left  == nullptr)
+                        {
+                            _node->left = temp;
+                            temp->parent = _node->left;
+                            check(temp);
+                        }
+                        else {
+                            _node->left = temp;
+                        }
                     }
                     else {
                         _node->right = _insert(_node->right, value);
