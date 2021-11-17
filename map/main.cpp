fdbd9aec512826f3b60f0de6ad08cfe5de206fb2
youcef <youssef.xd.12@gmail.com>
2021-11-17 12:25:32 +0100

    daf

--- map/main.cpp
+++ map/main.cpp
@@ -11,13 +11,17 @@ int main()
         // t->insert(3);
         // t->insert(4);
         t->insert(4);
-        t->insert(3);
         t->insert(2);
-        t->insert(1);
+        t->insert(5);
+        t->insert(-1);
         t->insert(0);
-        // t->insert(-5);
+        t->insert(9);
+        t->insert(24);
+        t->insert(14);
+        t->insert(99);
 
-        t->print();
+        // t->print();
+        std::cout << t->exist(99) <<   std::endl;
 
         // std::cout <<   "|" << t->root->value << std::endl << std::endl;
         // std::cout  << t->root->left->value  <<  std::endl << std::endl;
--- map/rbtree.hpp
+++ map/rbtree.hpp
@@ -79,6 +79,191 @@ template<
                     return  0;
                 return 1;
             }
+
+
+
+            
+                node<T> *_find(node<T> *n ,T val)
+                {
+                        if (n == nullptr)
+                            return  n;
+                        if (val < n->value)
+                            _find(n->left, val);
+                        else if (val > n->value)
+                            _find(n->right, val);
+                        else
+                            return n;
+                        return nullptr;
+                }
+
+                node<T> *find(T val)
+                {
+                    return _find(root, val);
+                }
+
+                bool exist(T val)
+                {
+                    return find(val) != nullptr;
+                }
+                
+
+
+                bool isNullLeaf(node<T> *n)
+                {
+                    return n== nullptr;
+                }
+
+                void _delete(node<T> *n , T val)
+                {
+                        if (root == nullptr || isNullLeaf(n))
+                            return ;
+                        if (n->value == val)
+                        {
+                            if (isNullLeaf(n->right) || isNullLeaf(n->left)){
+                                            deleteOneChild(n);
+                            
+                            }else {
+                                node<T> *next = findSamllest(n->right);
+                                n->value = next->value;
+                                _delete(n->right , next->value);
+                            }
+                        }
+                        if (n->value < val)
+                            _delete(n->right, val);
+                        else
+                            _delete(n->left, val);
+                }
+
+                node<T> *findSamllest(node<T> *n)
+                {
+                    node<T> *temp = nullptr;
+
+                    while (n != nullptr && !isNullLeaf(n))
+                    {
+                        temp = n;
+                        n = n->left;
+                    }
+                    return temp != nullptr ? temp : n;
+                }
+                void deleteOneChild(node<T> *n)
+                {
+                    if (n == nullptr)
+                    {
+                        std::cout << "null node " << std::endl;
+                        return ;
+                    }
+                    node<T> *toDelete = isNullLeaf(n->right) ? n->left : n->right;
+
+                    replaceNode(n, toDelete);
+
+                    if (toDelete->color == Black)
+                    {
+                        if (toDelete->color == Red)
+                        {
+                            toDelete->color = Black;
+                        }
+                        else {
+                            deleteCase1(toDelete, )
+                        }
+                    }
+                }
+
+                void deleteCase1(node<T> *n)
+                {
+                    if (n->parent == nullptr)
+                    {
+                        this->root = n;
+                        return ;
+                    }
+                    deleteCase2(n);
+                }
+                void deleteCase2(node<T> *n)
+                {
+                    node<T> *siblingNode = findSiblingNode(n);
+                    if (siblingNode->color == Red)
+                    {
+                        if (siblingNode->isRight == false)
+                        {
+                            rightRotate(siblingNode);
+                        }else {
+                            leftRotate(siblingNode);
+                        }
+
+                        if (siblingNode == root)
+                        {
+                            root = siblingNode;
+                        }
+                    }
+
+                deleteCase3(n);
+                }
+
+                void deleteCase3(node<T> *n)
+                {
+                    node <T> *sibling = findSiblingNode(n);
+                    if (n->parent->color == Black && sibling->color == Black && sibling->left->color == Black && sibling->right->color == Black)
+                    {
+                        sibling->color = Red;
+                        deleteCase1(n->parent);
+                    }else {
+                        deleteCase4(n);
+                    }
+
+                }
+                void deleteCase4(node<T> *n)
+                {                  
+                    node <T> *sibling = findSiblingNode(n);
+                    if (n->parent->color == Red && sibling->color == Black && sibling->left->color == Black && sibling->right->color == Black)
+                    {
+                        sibling->color = Red;
+                        n->color = Black;
+                    }else {
+                        deleteCase5(n);
+                    }
+
+                }
+                void deleteCase5(node<T> *n)
+                {
+                    node <T> *sibling = findSiblingNode(n);
+
+                    if (sibling->color == Black)
+                    {
+                        if (n->isRight == false && sibling->right->color== Black && sibling->left->color == Red)
+                        {
+                            rightRotate(sibling->left);
+                        }else if (n->isRight && sibling->left->color == Black && sibling->right->color ==  Black){
+                                leftRoutate(sibling->right);
+                        }
+                    }
+                    deleteCase6(n);
+                }
+                void deleteCase6(node<T> *n)
+                {
+                    node <T> *sibling = findSiblingNode(n);
+                    sibling->color = sibling->parent->color;
+                    sibling->parent->color = Black;
+                    if(n->isRight == false)
+                    {
+                        sibling->right->color = Black;
+                        leftRoutate(sibling);
+                    }else {
+                        sibling->left->color = Black;
+                        rightRotate(sibling);
+                    }
+                    if (sibling->parent == nullptr)
+                    {
+                        root = sibling;
+                    }
+
+                }
+
+                void delete_node(node<T> *n)
+                {
+                    node<T> *toDelete = 
+
+                }
+
+
             node<T>  *root;
             
 
@@ -89,7 +274,6 @@ template<
 
             void rightRotate(node<T> *n)
             {
-                std::cout <<  n->value <<"right rotate"  << std::endl;
                 if (n == nullptr)
                         return ;
 
@@ -127,9 +311,9 @@ template<
 
             void leftRoutate(node<T> *n)
             {                                
-                std::cout << "=====left rotate"  <<  n->right->value << std::endl;
                 if (n == nullptr)
                     return ;                    
+                std::cout << "=====left rotate"  <<  n->value << std::endl;
                 node<T> *temp = n->right;
                 if (temp == nullptr)
                     return ;
@@ -164,17 +348,19 @@ template<
 
             void leftRightRotate(node<T> *n)
             {
-                std::cout << "left right rotate" << std::endl;
+                std::cout <<"left right rotate" << std::endl;
                 if (n == nullptr)
                     return ;
-                leftRoutate(n->parent);
+                leftRoutate(n->left);
 
                 rightRotate(n);
             }
          void    rightleftRotate (node<T> *n)
             {
                 std::cout << "right left rotate" << std::endl;
-                rightRotate(n->parent);
+                if (n == nullptr)
+                        return ;
+                rightRotate(n->right);
                 leftRoutate(n);
             }
 
@@ -209,7 +395,7 @@ template<
                         return ;
                 }
                 leftRightRotate(n->parent->parent);
-                
+                // std::cout << "weee" <<   n->parent->parent  << std::endl;
                 n->color = Black;
                 if (n->right !=nullptr)
                     n->right->color = Red;
@@ -264,6 +450,8 @@ template<
             }
             void check(node<T> *n)
             {
+                if (n == nullptr)
+                        return;
                   if ( n == this->root )
                   {
                       correct(n);
@@ -271,7 +459,7 @@ template<
                   }
    
                 
-                if (n->color == Red && (n->parent->color == Red))
+                if (n->color == Red && ( n->parent &&  n->parent->color == Red))
                 {
                     if (n->parent->parent != nullptr)
                     {
